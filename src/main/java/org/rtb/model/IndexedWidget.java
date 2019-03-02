e5d91988b84ee191f2236c2b98704f8a0fa49e22
Wise <roman_nagaev_1993@mail.ru>
2019-03-02 19:25:10 +0500

    подготовил модели

---
+++ src/main/java/org/rtb/model/IndexedWidget.java
@@ -0,0 +1,16 @@
+package org.rtb.model;
+
+import lombok.AllArgsConstructor;
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter
+@Setter
+@EqualsAndHashCode(callSuper = true)
+public class IndexedWidget extends Widget{
+  private Integer z;
+}
---
+++ src/main/java/org/rtb/model/Widget.java
@@ -0,0 +1,18 @@
+package org.rtb.model;
+
+import lombok.AllArgsConstructor;
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter
+@Setter
+@EqualsAndHashCode
+public class Widget {
+  private Integer id;
+  private Integer x;
+  private Integer y;
+}
